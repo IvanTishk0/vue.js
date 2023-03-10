6a261384284e8491db9fbac70933c29a77ef504b
Ivan Tishko <ivantishko29@gmail.com>
2023-03-10 22:51:42 +0300

    second commit

---
+++ front-vue/src/components/Counter/Counter.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {reactive, ref, watch, watchEffect} from "vue";
+
+const counter = ref(0)
+const date = ref(new Date().toLocaleTimeString())
+watchEffect(() => {
+  setInterval(() => {
+    date.value = new Date().toLocaleTimeString()
+  }, 1000)
+})
+</script>
+
+<template>
+  <button @click="counter++">counter {{counter}}</button>
+  <h1>{{date}}</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
